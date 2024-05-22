e1296ea044bcd790c9eb7bc51a153c95a216610e
AlonsoPaz12 <alonsocpaz12g@gmail.com>
2024-05-21 19:58:53 -0500

    actualizando funcionalidades

--- frontend/src/app/ImagenesMedicas/page.jsx
+++ frontend/src/app/ImagenesMedicas/page.jsx
@@ -3,10 +3,26 @@
 import React, { useState } from 'react';
 import styles from './page.module.css';
 import LeftBar from '@/components/LeftBar/LeftBar';
+import Button from 'react-bootstrap/Button';
 import ProfileCard from '@/components/ProfileCard/ProfileCard';
 import ImagenesCard from '@/components/ImagenesCard/ImagenesCard';
+import AddInforImagenes from '@/components/AddInforImagenes/AddInforImagenes';
 
 const ImagenesMedicas = () => {
+  const [showCard, setshowCard] = useState(false);
+  const [imagen, setImagen] = useState([]);
+
+  const viewPageImage = () => setshowCard(true);
+  const closePageImage = () => setshowCard(false);
+
+  const viewSaveImage = (NewImagen) => {
+    setImagen(NewImagen);
+  };
+
+  const viewBorrarImage = (index) => {
+    const updateImage = imagen.filter((_, i) => i !== index);
+    setImagen(updateImage);
+  };
 
   return (
     <div className={styles.container}>
@@ -16,16 +32,29 @@ const ImagenesMedicas = () => {
           <h5 style={{marginTop:'80px'}}><b>MIS IMÁGENES MÉDICAS</b></h5>
           <ProfileCard/>
         </div>
-        <div class={styles.cardlabel}>
-            <input className={styles.inputlabal} type="text" required/>
+        <div className={styles.cardlabel}>
+            <input className={styles.inputlabal} type="text" required />
             <div className={styles.labelline}>Buscar por nombre de análisis</div>
-        </div>
+          </div>
         <div className={styles.cardbody}>
-          <ImagenesCard/>
-          <ImagenesCard/>
-          <ImagenesCard/>
+            {imagen.map((imagen, index) => (
+              <ImagenesCard
+                key={index}
+                imagen={imagen}
+                onDelete={() => viewBorrarImage(index)}
+              />
+            ))}
+        </div>
+        <div className={styles.footer}>
+          <Button variant="dark" className={styles.agregarImagen} onClick={viewPageImage}>Agregar y Editar Diagnostico</Button>
         </div>
       </div>
+      <AddInforImagenes
+        show={showCard}
+        closePage={closePageImage}
+        viewSave={viewSaveImage}
+        initialImageData={imagen}
+      />
     </div>
   );
 };
